1892e17176335e4ff3879346e2e8562e27a04a53
Kraaven <dhruv.bhojaraj@gmail.com>
2024-05-29 12:05:32 +0530

    Added Raylib
    Created a basic scripts to get things going:
    - Added the gameobject class
    - Added the ability to "render the object"
    - Right now, it just renders a simple line

---
+++ RayGame/GameObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using Raylib_cs;
+
+namespace RayGame;
+
+public class GameObject
+{
+    public Vector2 Position;
+    public float Rotation;
+
+    void InitValues()
+    {
+        Position = new Vector2();
+        Rotation = new float();
+    }
+
+    public GameObject()
+    {
+        InitValues();
+    }
+
+    public void RenderObject()
+    {
+        Raylib.DrawLineV(Position,Position+(Vector2.One*25),Color.Black);
+    }
+}
--- RayGame/Program.cs
+++ RayGame/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Numerics;
+using RayGame;
+using Raylib_cs;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Raylib.InitWindow(800,480,"Hello World");
+        GameObject test = new GameObject();
+        test.Position = new Vector2(200, 200);
+        
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+            test.RenderObject();
+            Raylib.EndDrawing();
+        }
+        
+        Raylib.CloseWindow();
+    }
 
-Console.WriteLine("Hello, World!");
+}
